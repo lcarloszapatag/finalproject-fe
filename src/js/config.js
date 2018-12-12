0066242809eeddbff3965b65f59bce77dd7c2b4b
Martin Arzuaga <martinarzuagafe@gmail.com>
2018-12-12 17:32:00 -0300

    Commit Initial: arquitecture all type of files and documments.

---
+++ src/js/config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+
+const config = {
+  apiKey: "AIzaSyDVb-4rIW2GK7dGcABTcOV7_eRpMXtvD9w",
+  authDomain: "proyecto-final-edteam.firebaseapp.com",
+  databaseURL: "https://proyecto-final-edteam.firebaseio.com",
+  projectId: "proyecto-final-edteam",
+  storageBucket: "",
+  messagingSenderId: "482025817687"
+}
+
+const init = () => firebase.initializeApp(config)
+
+init()
